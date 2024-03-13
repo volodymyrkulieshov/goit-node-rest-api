e396f0db14e21c6437d4482a440ceb5a1baa8560
Volodymyr <vovkul@gmail.com>
2024-03-13 23:15:26 +0100

    Update contactsControllers.js

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -3,17 +3,20 @@ import HttpError from "../helpers/HttpError.js";
 
 export const getAllContacts = async (req, res, next) => {
   try {
-  const { _id: owner } = req.user;
-  const { page = 1, limit = 20, favorite } = req.query;
-  const filter = { owner };
-  if (favorite === "true") {
-    filter.favorite = true;
-  } else if (favorite === "false") {
-    filter.favorite = false;
-  }
-  const skip = (page - 1) * limit;
-  const contactsList = await ContactsModel.find(filter, "", { skip, limit });
-  res.json(contactsList);
+    const { _id: owner } = req.user;
+    const { page = 1, limit = 20, favorite } = req.query;
+    const filter = { owner };
+    if (favorite === "true") {
+      filter.favorite = true;
+    } else if (favorite === "false") {
+      filter.favorite = false;
+    }
+    const skip = (page - 1) * limit;
+    const contactsList = await ContactsModel.find(filter, "", {
+      skip,
+      limit,
+    }).populate("owner", "name email");
+    res.json(contactsList);
   } catch (error) {
     next(error);
   }
@@ -21,12 +24,15 @@ export const getAllContacts = async (req, res, next) => {
 
 export const getOneContact = async (req, res, next) => {
   try {
-  const { id } = req.params;
-  const searchedContact = await ContactsModel.findById(id);
-  if (!searchedContact) {
-    throw HttpError(404);
-  }
-  res.json(searchedContact);
+    const { id } = req.params;
+    const { _id: owner } = req.user;
+    const searchedContact = await ContactsModel.findById({ _id: id })
+      .where("owner")
+      .equals(owner);
+    if (!searchedContact) {
+      throw HttpError(404);
+    }
+    res.json(searchedContact);
   } catch (error) {
     next(error);
   }
@@ -34,9 +40,9 @@ export const getOneContact = async (req, res, next) => {
 
 export const createContact = async (req, res, next) => {
   try {
-  const { _id: owner } = req.user;
-  const newContact = await ContactsModel.create({ ...req.body, owner });
-  res.status(201).json(newContact);
+    const { _id: owner } = req.user;
+    const newContact = await ContactsModel.create({ ...req.body, owner });
+    res.status(201).json(newContact);
   } catch (error) {
     next(error);
   }
@@ -45,16 +51,19 @@ export const createContact = async (req, res, next) => {
 export const updateContact = async (req, res, next) => {
   try {
     if (Object.keys(req.body).length === 0) {
-          throw HttpError(400, "Body must have at least one field");
-        }
-        const { id } = req.params;
-        const updatedContact = await ContactsModel.findByIdAndUpdate(id, req.body, {
-          new: true,
-        });
-        if (!updatedContact) {
-          throw HttpError(404);
-        }
-        res.json(updatedContact);
+      throw HttpError(400, "Body must have at least one field");
+    }
+    const { id } = req.params;
+    const { _id: owner } = req.user;
+    const updatedContact = await ContactsModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    })
+      .where("owner")
+      .equals(owner);
+    if (!updatedContact) {
+      throw HttpError(404);
+    }
+    res.json(updatedContact);
   } catch (error) {
     next(error);
   }
@@ -81,12 +90,12 @@ export const updateStatusContact = async (req, res, next) => {
 
 export const deleteContact = async (req, res, next) => {
   try {
-  const { id } = req.params;
-  const removedContact = await ContactsModel.findByIdAndDelete(id);
-  if (!removedContact) {
-    throw HttpError(404);
-  }
-  res.json(removedContact);
+    const { id } = req.params;
+    const removedContact = await ContactsModel.findByIdAndDelete(id);
+    if (!removedContact) {
+      throw HttpError(404);
+    }
+    res.json(removedContact);
   } catch (error) {
     next(error);
   }
